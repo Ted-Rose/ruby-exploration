3d2b75457df993e97a9e0eadedecea3e76e1c545
Tedis Rozenfelds <trozenfelds@gocardless.com>
2024-04-23 10:54:58 +0300

    test_sorbert

---
+++ library_tests/test_sorbert.rb
@@ -0,0 +1,13 @@
+require 'sorbet-runtime'
+
+extend T::Sig
+
+sig {params(name: String).returns(Integer)}
+def main(name)
+  puts "Hello, #{name}!"
+  name.length
+end
+
+main("Sorbet") # ok!
+main()   # error: Not enough arguments provided
+man("")  # error: Method `man` does not exist
