1ba1182ba67b45523e88eeb447482afb1e35e00d
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2025-01-10 14:27:20 +0200

    variable_references.rb

---
+++ other/variable_references.rb
@@ -0,0 +1,16 @@
+color = 'red'
+size = 12
+
+shoe_parameters = {
+  color:,
+  size:,
+}
+
+pp shoe_parameters
+
+color = 'green'
+
+pp shoe_parameters
+
+shoe_parameters[:color] = 'blue'
+pp shoe_parameters
